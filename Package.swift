d85ee80128d7cc36796c984463cfd75b1d38587d
Phisto <simon.cay.gaus@gmail.com>
2023-10-17 02:17:21 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-lame",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12)
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "lame",
+            targets: ["lame"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "lame"),
+        .testTarget(
+            name: "swift-lameTests",
+            dependencies: ["lame"]),
+    ]
+)
